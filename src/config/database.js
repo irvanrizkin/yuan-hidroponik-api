7a360719dc0606082f283efc87677172d19ae649
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-05-22 15:40:37 +0700

    feat: added migration for records table

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const databaseConfig = {
+  development: {
+    username: process.env.DEV_DB_USER,
+    password: process.env.DEV_DB_PASS,
+    database: process.env.DEV_DB_NAME,
+    host: process.env.DEV_DB_HOST,
+    port: process.env.DEV_DB_PORT,
+    dialect: 'mysql',
+  }
+}
+
+module.exports = databaseConfig;
---
+++ src/database/migrations/20230522074617-create-record.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('records', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ppm: {
+        type: Sequelize.INTEGER
+      },
+      temperature: {
+        type: Sequelize.DECIMAL
+      },
+      source: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('records');
+  }
+};
